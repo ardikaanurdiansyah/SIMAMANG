aac21743ac74a54951f0c25463772b7ac45678c8
ardikaanurdiansyah <dikalokers0@gmail.com>
2026-08-05 13:58:44 +0700

    Initial commit SIMAMANG

---
+++ database/migrations/2026_08_05_064853_create_peserta_magangs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peserta_magangs', function (Blueprint $table) {
+    $table->id();
+    $table->string('nama');
+    $table->string('asal_instansi');
+    $table->string('jurusan');
+    $table->string('no_hp');
+    $table->string('email')->nullable();
+
+    $table->foreignId('divisi_id')->constrained('divisis')->onDelete('cascade');
+
+    $table->date('tanggal_mulai');
+    $table->date('tanggal_selesai');
+
+    $table->enum('status', ['Aktif', 'Selesai'])->default('Aktif');
+
+    $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peserta_magangs');
+    }
+};
