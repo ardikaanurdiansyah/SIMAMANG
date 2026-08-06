d62056b44db3cffbeac17cb26b4deb0df17ed0ab
novalrediansyah127-blip <novalrediansyah127@gmail.com>
2026-08-06 14:45:44 +0700

    Tambah Login

--- database/migrations/2026_08_05_064853_create_peserta_magangs_table.php
+++ database/migrations/2026_08_05_064853_create_peserta_magangs_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    { 
         Schema::create('peserta_magangs', function (Blueprint $table) {
     $table->id();
     $table->string('nama');
