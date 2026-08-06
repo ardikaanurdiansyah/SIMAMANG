0c7a58c2fd7379df8f04f2f91f81a0b9f6675abf
Keizaraghaiza <keizaraghaiza761@gmail.com>
2026-08-06 11:17:42 +0700

    tambah web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PesertaMagangController;
 use Illuminate\Support\Facades\Route;
 
