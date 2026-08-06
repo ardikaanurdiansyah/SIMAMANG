8074deed06865c98f1f74f1d3f33aac325351920
Keizaraghaiza <keizaraghaiza761@gmail.com>
2026-08-06 10:41:46 +0700

    tambah web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\PesertaMagangController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/dashboard', [PesertaMagangController::class, 'dashboard'])->name('dashboard');
+
+    Route::get('/peserta/create', [PesertaMagangController::class, 'create'])->name('peserta.create');
+    Route::post('/peserta', [PesertaMagangController::class, 'store'])->name('peserta.store');
 });
+
+Route::get('/', fn () => redirect()->route('dashboard'));
